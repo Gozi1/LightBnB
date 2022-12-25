62d68b0b1315e09d8d67b063c260418f89470cd6
Gozi1 <snwabiani@gmail.com>
2022-12-25 22:31:04 +0000

    refactored functions getUserWithEmail and getUserWithId to use the SQL database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -17,16 +17,20 @@ const users = require('./json/users.json');
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  const queryString = `
+  SELECT *
+  FROM users 
+  WHERE email = $1;
+  `;
+  return pool
+  .query(queryString,
+    [email])
+  .then((result) => {
+    return result.rows[0]
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -36,7 +40,20 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  const queryString = `
+  SELECT *
+  FROM users 
+  WHERE id = $1;
+  `;
+  return pool
+  .query(queryString,
+    [id])
+  .then((result) => {
+    return result.rows[0]
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 }
 exports.getUserWithId = getUserWithId;
 
@@ -84,7 +101,7 @@ const getAllProperties = function(options, limit = 10) {
   .query(queryString,
     [limit])
   .then((result) => {
-    console.log(result.rows);
+    
     return result.rows;
   })
   .catch((err) => {
