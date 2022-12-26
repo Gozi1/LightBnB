864448eb50501bf94727035bad705fa4ea0cf899
Gozi1 <snwabiani@gmail.com>
2022-12-26 18:41:50 +0000

    refactor function addUser to use sql

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -64,10 +64,22 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  console.log(user);
+  const queryString = `
+  INSERT INTO users 
+  (name, email, password) 
+  VALUES ($1,$2,$3) RETURNING id;
+  `;
+  return pool
+  .query(queryString,
+    [user.name,user.email,user.password])
+  .then((result) => {
+    return result.rows[0]
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+  
 }
 exports.addUser = addUser;
 
