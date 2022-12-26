a66dfba1d0b06763c59ebf74c497b735426665ef
Gozi1 <snwabiani@gmail.com>
2022-12-26 21:38:11 +0000

    removed a console.log debugger msg

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -167,10 +167,6 @@ const getAllProperties = function(options, limit = 10) {
    ORDER BY cost_per_night
    LIMIT $${queryParams.length};
    `;
-  
-   
-   console.log(queryString, queryParams);
- 
    return pool.query(queryString, queryParams).then((res) => res.rows)
   .catch((err) => {
     console.log(err.message);
